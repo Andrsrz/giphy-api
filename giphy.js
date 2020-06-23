f8e9d16dbc9d3c47e842add452d6947e774f5466
Andres Ruiz <andrsruiz@gmail.com>
2020-06-23 10:16:38 -0500

    Implement Search Bar Method

--- giphy.js
+++ giphy.js
@@ -1,8 +1,25 @@
 const img = document.querySelector('img');
+const btn = document.getElementById('getgif');
+const search = document.getElementById('search');
+const url = "https://api.giphy.com/v1/gifs/translate?api_key=EGV8DNdpU1bmKERUsuZPVJlC4wjaUVEB&s=";
+const cat = "cat";
 
-fetch("https://api.giphy.com/v1/gifs/translate?api_key=EGV8DNdpU1bmKERUsuZPVJlC4wjaUVEB&s=cats", {
+const setButtonEvent = () => {
+	btn.addEventListener('click', () => {
+		let newUrl = url + search.value;
+		fetchApi(newUrl);
+	}, false);
+}
+
+const fetchApi = (url) => {
+	fetch(url, {
 	mode: 'cors'}).then((res) => {
 		return res.json();
 	}).then((res) => {
 		img.src = res.data.images.original.url;
 	});
+}
+
+// Default Gif
+fetchApi(url+cat);
+setButtonEvent();
